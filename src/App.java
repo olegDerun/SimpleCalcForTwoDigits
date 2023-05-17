60e289b999e02dfa109b4b80ba0be12babc8fb86
Oleg Derun <oleg.derun21@gmail.com>
2023-05-18 00:19:19 +0300

    Add some calculation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(10+20);
+        System.out.println(10-20);
+        System.out.println(10/20);
+        System.out.println(10*20);
+        System.out.println(10%20);
     }
 }
